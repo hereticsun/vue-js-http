6ef36ab819ec226829ea961bc455331f1a3f66aa
Azlan Cuttilan <azlan@bynd.com>
2017-01-24 10:51:59 +0000

    POSTing data to the server
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input class="form-control" type="text" v-model="user.username"></input>
+                </div>
+                <div class="form-group">
+                    <label>Mail</label>
+                    <input class="form-control" type="text" v-model="user.email"></input>
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                user: {
+                    username: '',
+                    email: ''
+                }
+            };
+        },
+        methods: {
+            submit() {
+                this.$http.post('https://vuejs-http-53565.firebaseio.com/data.json', this.user)
+                    .then( response => {
+                        console.log(response)
+                    }, error => {
+                        console.log(error);
+                    });
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
