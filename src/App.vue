f333fdddb2b7875aeab25f3cadb55a77bdfea2ab
Azlan Cuttilan <azlan@bynd.com>
2017-01-24 12:27:00 +0000

    Understanding template urls
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr/>
+                <input class="form-control" type="text" v-model="node">
+                <br/><br/>
                 <button class="btn btn-primary" @click="fetchData">Get Data</button>
                 <ul class="list-group">
                     <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
@@ -31,7 +33,8 @@
                     email: ''
                 },
                 users: [],
-                resource: []
+                resource: [],
+                node: 'data'
             };
         },
         methods: {
@@ -48,7 +51,20 @@
                 this.resource.saveAlt(this.user);
             },
             fetchData() {
+/*
                 this.$http.get('data.json')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for(let key in data) {
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
+                    });
+*/
+                this.resource.getData({node: this.node})
                     .then(response => {
                         return response.json();
                     })
@@ -63,9 +79,10 @@
         },
         created() {
             const customActions = {
-                saveAlt: {method: 'POST', url: 'alternative.json'}
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET'}
             }
-            this.resource = this.$resource('data.json', {}, customActions);
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
     }
 </script>
